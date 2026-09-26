ef071efeacd5c6467dbb117527d00ec49c64dda0
SOHAM CHINTAWAR <sohamchintawar111@gmail.com>
2026-09-26 22:16:00 +0530

    Update Cart.js

--- backend/models/Cart.js
+++ backend/models/Cart.js
@@ -5,6 +5,7 @@ module.exports = (sequelize, DataTypes) => {
       defaultValue: DataTypes.UUIDV4,
       primaryKey: true,
     },
+    
     userId: {
       type: DataTypes.UUID,
       allowNull: false,
@@ -14,24 +15,28 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.FLOAT,
       defaultValue: 0,
     },
-  }, {
+    
+  }, 
+    {
     tableName: 'carts',
     timestamps: true,
   });
 
-  // Helper to recalculate total price from cart items
+  // Helper to recalculate total price from cart items  
   Cart.prototype.recalculateTotal = async function () {
     const { CartItem } = require('./index');
     const items = await CartItem.findAll({ where: { cartId: this.id } });
     this.totalPrice = items.reduce((sum, item) => sum + (item.price * item.quantity), 0);
     await this.save();
   };
+  
 
   Cart.prototype.toJSON = function () {
     const values = { ...this.get() };
     values._id = values.id;
     return values;
   };
+  
 
   return Cart;
 };
